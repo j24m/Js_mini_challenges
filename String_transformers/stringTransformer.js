1d91f3f433a8c9e67d74f13a9c65c31972c4fa70
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2024-07-25 23:59:48 +0530

    feat : added upper and lower case transform

--- String_transformers/stringTransformer.js
+++ String_transformers/stringTransformer.js
@@ -1,1 +1,9 @@
 let userInput = document.querySelector(".input");
+let lowerCaseEl = document.querySelector("#lowercase");
+let upperCaseEl = document.querySelector("#uppercase");
+userInput.addEventListener("input", transform);
+function transform() {
+  let text = userInput.value;
+  lowerCaseEl.textContent = text.toLowerCase();
+  upperCaseEl.textContent = text.toUpperCase();
+}
